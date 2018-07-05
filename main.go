bc6fd651497ef8526a8e85c487fb62b9e1f5dd6e
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2018-07-05 14:25:32 +0300

    Lots of improvements to the tool

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"bytes"
 	"compress/gzip"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"regexp"
 	"strings"
+	"sync"
 )
 
 type server struct{}
@@ -25,45 +26,67 @@ var client = &http.Client{
 	},
 }
 
+var ignoredHeaders = map[string]struct{}{
+	// content length will change after stripping and needs to be recalculated
+	"Content-Length": struct{}{},
+	// hsts and hpkp headers
+	"Public-Key-Pins":             struct{}{},
+	"Public-Key-Pins-Report-Only": struct{}{},
+	"Strict-Transport-Security":   struct{}{},
+}
+
 var storedLinks map[clientLink]string = make(map[clientLink]string, 0)
+var mu sync.RWMutex
 
-func getCookieKey(cookie string) string {
-	return strings.SplitN(cookie, "=", 2)[0]
+func getLink(cl clientLink) (string, bool) {
+	mu.RLock()
+	defer mu.RUnlock()
+	link, exists := storedLinks[cl]
+
+	// for key, val := range storedLinks {
+	// 	fmt.Printf("RemoteAddr: %q\t%q\t%q\n", key.clientIP, key.url, val)
+	// }
+	// fmt.Println()
+
+	return link, exists
 }
 
-func getHeaderWithCookies(oldHeaders http.Header, newHeaders http.Header) http.Header {
-	if cookies, exists := newHeaders["Set-Cookie"]; exists {
-		for _, value := range cookies {
-			shouldOverwrite := false
-			for i, cookie := range oldHeaders["Cookie"] {
-				if getCookieKey(cookie) == getCookieKey(value) {
-					oldHeaders["Cookie"][i] = value
-					shouldOverwrite = true
-					break
-				}
-			}
-
-			if !shouldOverwrite {
-				oldHeaders["Cookie"] = append(oldHeaders["Cookie"], value)
-			}
-		}
-	}
-
-	return oldHeaders
+func setLink(cl clientLink, link string) {
+	mu.Lock()
+	defer mu.Unlock()
+	storedLinks[cl] = link
 }
 
-func makeRequest(req *http.Request, collectedCookies []string, redirectCount int) (*http.Response, error) {
-	if redirectCount >= 10 {
-		return nil, errors.New("stopped after 10 redirects")
+func normalizeIP(remoteAddr string) string {
+	return strings.Split(remoteAddr, ":")[0]
+}
+
+func normalizeUrl(link string) (string, error) {
+	originalUrl, err := url.Parse(link)
+
+	if err != nil {
+		return "", err
 	}
+
+	if originalUrl.Path == "" {
+		originalUrl.Path = "/"
+	}
+
+	return originalUrl.String(), nil
+}
+
+func makeRequest(req *http.Request) (*http.Response, error) {
+	// error will never happen
+	u, _ := normalizeUrl(req.URL.String())
 
 	cl := clientLink{
-		clientIP: req.RemoteAddr,
-		url:      req.URL.String(),
+		clientIP: normalizeIP(req.RemoteAddr),
+		url:      u,
 	}
 
-	if _, exists := storedLinks[cl]; exists {
-		originalUrl, err := req.URL.Parse(storedLinks[cl])
+	// restore original link if cached
+	if link, exists := getLink(cl); exists {
+		originalUrl, err := req.URL.Parse(link)
 
 		if err != nil {
 			return nil, err
@@ -72,92 +95,77 @@ func makeRequest(req *http.Request, collectedCookies []string, redirectCount int
 		req.URL = originalUrl
 	}
 
+	// make request to server
 	res, err := client.Do(req)
 
 	if err != nil {
 		return nil, err
 	}
 
-	var redirectMethod string
-	var shouldRedirect, includeBody bool
-
-	switch res.StatusCode {
-	case 301, 302, 303:
-		redirectMethod = req.Method
-		shouldRedirect = true
-		includeBody = false
-
-		// RFC 2616 allowed automatic redirection only with GET and
-		// HEAD requests. RFC 7231 lifts this restriction, but we still
-		// restrict other methods to GET to maintain compatibility.
-		// See Issue 18570.
-		if req.Method != "GET" && req.Method != "HEAD" {
-			redirectMethod = "GET"
-		}
-	case 307, 308:
-		redirectMethod = req.Method
-		shouldRedirect = true
-		includeBody = true
-
-		// Treat 307 and 308 specially, since they're new in
-		// Go 1.8, and they also require re-sending the request body.
-		if res.Header.Get("Location") == "" {
-			// 308s have been observed in the wild being served
-			// without Location headers. Since Go 1.7 and earlier
-			// didn't follow these codes, just stop here instead
-			// of returning an error.
-			// See Issue 17773.
-			shouldRedirect = false
-			break
-		}
-		// if req.GetBody == nil && req.outgoingLength() != 0 {
-		// 	// We had a request body, and 307/308 require
-		// 	// re-sending it, but GetBody is not defined. So just
-		// 	// return this response to the user instead of an
-		// 	// error, like we did in Go 1.7 and earlier.
-		// 	shouldRedirect = false
-		// }
-	}
-
-	if shouldRedirect {
-		loc := res.Header.Get("Location")
-		if loc == "" {
-			if res.Body != nil {
-				res.Body.Close()
-			}
-			return nil, errors.New("wrong location redirect")
-		}
-
-		url, err := req.URL.Parse(loc)
-		if err != nil {
-			if res.Body != nil {
-				res.Body.Close()
-			}
-			return nil, fmt.Errorf("failed to parse Location header %q: %v", loc, err)
-		}
-
-		proxyReq := &http.Request{
-			Method: redirectMethod,
-			URL:    url,
-			Header: getHeaderWithCookies(req.Header, res.Header),
-			Close:  req.Close,
-		}
-
-		collectedCookies := append(collectedCookies, res.Header["Set-Cookie"]...)
-
-		if includeBody {
-			proxyReq.Body = req.Body
-		}
-
-		return makeRequest(proxyReq, collectedCookies, redirectCount+1)
-	}
-
-	res.Header["Set-Cookie"] = append(res.Header["Set-Cookie"], collectedCookies...)
-
-	return res, err
+	return res, nil
 }
 
-func (s server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func stripResponse(req *http.Request, res *http.Response) ([]byte, error) {
+	// strip location header if exists
+	location := res.Header.Get("Location")
+	if strings.HasPrefix(location, "https") {
+		strippedLocation, err := normalizeUrl("http" + location[5:])
+
+		if err != nil {
+			return nil, err
+		}
+
+		// cache original location
+		forgedKey := clientLink{
+			clientIP: normalizeIP(req.RemoteAddr),
+			url:      strippedLocation,
+		}
+		setLink(forgedKey, location)
+		res.Header.Set("Location", strippedLocation)
+	}
+
+	// strip secure cookies
+	if cookies, exists := res.Header["Set-Cookie"]; exists {
+		for i := 0; i < len(cookies); i++ {
+			cookie := cookies[i]
+			if idx := strings.LastIndex(cookie, "Secure"); idx != -1 {
+				cookies[i] = cookie[:idx] + cookie[idx+6:]
+			}
+		}
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, fmt.Errorf("Error when reading response body: %q\n", err)
+	}
+
+	// strip all https links in the response body
+	regex, _ := regexp.Compile("(https://[a-zA-Z0-9_:#@%/;$()~_?+-=\\.&]*)")
+	strippedBody := regex.ReplaceAllFunc(body, func(u []byte) []byte {
+		url := string(u)
+		strippedUrl, err := normalizeUrl("http://" + url[8:])
+
+		if err != nil {
+			fmt.Printf("Warning: could not normalize url %s: %q\n", url, err)
+			return u
+		}
+
+		// store original link for future requests
+		key := clientLink{
+			clientIP: normalizeIP(req.RemoteAddr),
+			url:      strippedUrl,
+		}
+		setLink(key, url)
+		return []byte(strippedUrl)
+	})
+
+	return strippedBody, nil
+}
+
+func (s server) ServeHTTP(responseWriter http.ResponseWriter, req *http.Request) {
+	// read request body
 	reqBody, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		fmt.Println("Could not get body")
@@ -165,93 +173,99 @@ func (s server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 
 	fmt.Println("Incoming request:")
-	fmt.Printf("Method: %q\n", req.Method)
-	fmt.Printf("URL: %q\n", req.URL)
-	fmt.Printf("Proto, ProtoMajor, ProtoMinor: %q %q %q\n", req.Proto, req.ProtoMajor, req.ProtoMinor)
-	fmt.Printf("Header: %q\n", req.Header)
-	fmt.Printf("Host: %q\n", req.Host)
-	fmt.Printf("Content-Length: %q\n", req.ContentLength)
-	fmt.Printf("Transfer-encoding: %q\n", req.TransferEncoding)
-	fmt.Printf("Close: %v\n", req.Close)
-	fmt.Printf("Form: %q\n", req.Form)
-	fmt.Printf("PostForm: %q\n", req.PostForm)
-	fmt.Printf("MultipartForm: %q\n", req.MultipartForm)
-	fmt.Printf("Trailer: %q\n", req.Trailer)
-	fmt.Printf("RemoteAddr: %q\n", req.RemoteAddr)
-	fmt.Printf("RequestUri: %q\n", req.RequestURI)
-	fmt.Printf("Body: %q\n", reqBody)
-	fmt.Println("")
+	// fmt.Printf("Method: %q\n", req.Method)
+	// fmt.Printf("URL: %q\n", req.URL)
+	// fmt.Printf("Proto, ProtoMajor, ProtoMinor: %q %q %q\n", req.Proto, req.ProtoMajor, req.ProtoMinor)
+	// fmt.Printf("Header: %q\n", req.Header)
+	// fmt.Printf("Host: %q\n", req.Host)
+	// fmt.Printf("Content-Length: %q\n", req.ContentLength)
+	// fmt.Printf("Transfer-encoding: %q\n", req.TransferEncoding)
+	// fmt.Printf("Close: %v\n", req.Close)
+	// fmt.Printf("Form: %q\n", req.Form)
+	// fmt.Printf("PostForm: %q\n", req.PostForm)
+	// fmt.Printf("MultipartForm: %q\n", req.MultipartForm)
+	// fmt.Printf("Trailer: %q\n", req.Trailer)
+	// fmt.Printf("RemoteAddr: %q\n", req.RemoteAddr)
+	// fmt.Printf("RequestUri: %q\n", req.RequestURI)
+	// fmt.Printf("Body: %q\n", reqBody)
+	// fmt.Println("")
 
+	// build request to be made
 	proxyReq := &http.Request{
-		Method: req.Method,
-		URL:    req.URL,
-		Header: req.Header,
-		Host:   req.Host,
-		Close:  req.Close,
-		Body:   ioutil.NopCloser(bytes.NewReader(reqBody)),
+		Method:     req.Method,
+		URL:        req.URL,
+		Header:     req.Header,
+		Host:       req.Host,
+		Close:      req.Close,
+		Body:       ioutil.NopCloser(bytes.NewReader(reqBody)),
+		RemoteAddr: req.RemoteAddr,
 	}
 
-	res, err := makeRequest(proxyReq, make([]string, 0), 1)
+	// make request to the server
+	res, err := makeRequest(proxyReq)
 
 	if err != nil {
-		fmt.Printf("Error: %q\n", err)
+		fmt.Printf("Error occurred when making proxy request: %q\n", err)
 		return
 	}
-
-	fmt.Println("Fetched response:")
-	fmt.Printf("Status: %q\n", res.Status)
-	fmt.Printf("StatusCode: %q\n", res.StatusCode)
-	fmt.Printf("Proto: %q\n", res.Proto)
-	fmt.Printf("ProtoMajor: %q\n", res.ProtoMajor)
-	fmt.Printf("ProtoMinor: %q\n", res.ProtoMinor)
-	fmt.Printf("Header: %q\n", res.Header)
-	fmt.Printf("Content-Length: %q\n", res.ContentLength)
-	fmt.Printf("Transfer-Encoding: %q\n", res.TransferEncoding)
-	fmt.Printf("Uncompressed: %v\n", res.Uncompressed)
-	fmt.Printf("Trailer: %v\n", res.Trailer)
-	fmt.Println("")
 
 	// decompress body before stripping if compressed
-	reader := res.Body
 	if res.Header.Get("Content-Encoding") == "gzip" {
-		reader, err = gzip.NewReader(res.Body)
-		defer reader.Close()
+		reader, err := gzip.NewReader(res.Body)
+		if err != nil {
+			fmt.Printf("Error when decompressing response body: %q\n", err)
+			return
+		}
+		res.Body = reader
 	}
 
-	body, err := ioutil.ReadAll(reader)
-
-	regex, _ := regexp.Compile("(https://[a-zA-Z0-9_:#@%/;$()~_?+-=\\.&]*)")
-	strippedBody := regex.ReplaceAllFunc(body, func(u []byte) []byte {
-		url := string(u)
-		strippedUrl := "http://" + string(u[8:])
-		cl := clientLink{
-			clientIP: req.RemoteAddr,
-			url:      strippedUrl,
-		}
-		storedLinks[cl] = url
-		return []byte(strippedUrl)
-	})
-
+	// strip response
+	strippedBody, err := stripResponse(req, res)
 	if err != nil {
-		fmt.Printf("Error: %q\n", err)
+		fmt.Printf("Error when stripping response: %q\n", err)
 		return
 	}
 
-	header := w.Header()
-	for key, value := range res.Header {
-		header[key] = value
-	}
-
-	w.WriteHeader(res.StatusCode)
 	// compress stripped body if necessary
 	if res.Header.Get("Content-Encoding") == "gzip" {
 		var b bytes.Buffer
 		writer := gzip.NewWriter(&b)
 		writer.Write(strippedBody)
+		writer.Flush()
 		writer.Close()
 		strippedBody = b.Bytes()
 	}
-	w.Write(strippedBody)
+
+	// fmt.Println("Fetched response:")
+	// fmt.Printf("Status: %q\n", res.Status)
+	// fmt.Printf("StatusCode: %q\n", res.StatusCode)
+	// fmt.Printf("Proto: %q\n", res.Proto)
+	// fmt.Printf("ProtoMajor: %q\n", res.ProtoMajor)
+	// fmt.Printf("ProtoMinor: %q\n", res.ProtoMinor)
+	// fmt.Printf("Header: %q\n", res.Header)
+	// fmt.Printf("Content-Length: %q\n", res.ContentLength)
+	// fmt.Printf("Transfer-Encoding: %q\n", res.TransferEncoding)
+	// fmt.Printf("Uncompressed: %v\n", res.Uncompressed)
+	// fmt.Printf("Trailer: %v\n", res.Trailer)
+	// fmt.Println("")
+
+	// build headers that will be returned to the client
+	header := responseWriter.Header()
+	for key, value := range res.Header {
+		if _, ignored := ignoredHeaders[key]; !ignored {
+			header[key] = value
+		}
+	}
+
+	responseWriter.WriteHeader(res.StatusCode)
+
+	// send stripped body to client
+	_, err = responseWriter.Write(strippedBody)
+
+	if err != nil {
+		fmt.Printf("Error when sending response body to client: %q\n", err)
+		return
+	}
 }
 
 func main() {
